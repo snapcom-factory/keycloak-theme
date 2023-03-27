de174a141c5ae9f001143b17015593a94fec2423
Robin Minervini <robin.minervini@gmail.com>
2023-03-27 20:27:56 +0200

    link account page to core and add inputs

--- src/ui-dsfr/pages/Account/Account.tsx
+++ src/ui-dsfr/pages/Account/Account.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useReducer } from "react";
 import { createGroup, type Route } from "type-route";
 import { routes } from "ui-dsfr/routes";
 import { makeStyles } from "tss-react/dsfr";
@@ -7,8 +7,10 @@ import { useTranslation } from "ui-dsfr/i18n";
 import { assert } from "tsafe/assert";
 import { Equals } from "tsafe";
 import { declareComponentKeys } from "i18nifty";
-import Display from "@codegouvfr/react-dsfr/Display";
-import { useCoreFunctions } from "core-dsfr";
+import { useCoreFunctions, useCoreState } from "core-dsfr";
+import { Input } from "@codegouvfr/react-dsfr/Input";
+import { z } from "zod";
+import { AutocompleteInput } from "ui-dsfr/shared/AutocompleteInput";
 
 Account.routeGroup = createGroup([routes.account]);
 type PageRoute = Route<typeof Account.routeGroup>;
@@ -26,38 +28,149 @@ export function Account(props: Props) {
     assert<Equals<typeof rest, {}>>();
 
     const { userAuthentication } = useCoreFunctions();
-    const [agencyNames, setAgencyNames] = useState<string[]>([]);
-    const [keycloakAccountConfigurationUrl, setKeycloakAccountConfigurationUrl] =
-        useState();
 
     const { classes, cx } = useStyles();
     const { t } = useTranslation({ Account });
 
     useEffect(() => {
-        userAuthentication.getAgencyNames().then(names => {
-            setAgencyNames(names);
-        });
+        triggerFetchAgencyNames();
     }, []);
+
+    const { agencyNames, triggerFetchAgencyNames } = (function useClosure() {
+        const [agencyNames, setAgencyNames] = useState<string[]>([]);
+        const [isTriggered, triggerFetchAgencyNames] = useReducer(() => true, false);
+
+        useEffect(() => {
+            if (!isTriggered) {
+                return;
+            }
+
+            let isCleanedUp = false;
+
+            userAuthentication.getAgencyNames().then(agencyNames => {
+                if (isCleanedUp) {
+                    return;
+                }
+
+                //NOTE: Just so that we do not have infinite loading for the first user
+                if (agencyNames.length === 0) {
+                    agencyNames = [""];
+                }
+
+                setAgencyNames(agencyNames);
+            });
+
+            return () => {
+                isCleanedUp = true;
+            };
+        }, [isTriggered]);
+
+        return { agencyNames, triggerFetchAgencyNames };
+    })();
+
+    const { allowedEmailRegexp } = (function useClosure() {
+        const [allowedEmailRegexp, setAllowedEmailRegexp] = useState<RegExp | undefined>(
+            undefined
+        );
+
+        useEffect(() => {
+            let isCleanedUp = false;
+
+            userAuthentication.getAllowedEmailRegexp().then(allowedEmailRegexp => {
+                if (isCleanedUp) {
+                    return;
+                }
+
+                setAllowedEmailRegexp(allowedEmailRegexp);
+            });
+
+            return () => {
+                isCleanedUp = true;
+            };
+        }, []);
+
+        return { allowedEmailRegexp };
+    })();
+
+    const { value: agencyName } = useCoreState(
+        state => state.userAuthentication.agencyName
+    );
+
+    const { value: email } = useCoreState(state => state.userAuthentication.email);
+
+    const keycloakAccountConfigurationUrl =
+        userAuthentication.getKeycloakAccountConfigurationUrl();
+
+    const onRequestUpdateFieldFactory = (
+        fieldName: "agencyName" | "email",
+        value: string
+    ) => userAuthentication.updateField({ fieldName, value });
+
+    const isValidEmail = (value: string) => {
+        assert(allowedEmailRegexp !== undefined);
+
+        try {
+            z.string().email().parse(value);
+        } catch {
+            return {
+                "isValidValue": false,
+                "message": "invalid email"
+            };
+        }
+
+        if (!allowedEmailRegexp.test(value)) {
+            return {
+                "isValidValue": false,
+                "message": "Your email domain isn't allowed yet"
+            };
+        }
+
+        return { "isValidValue": true };
+    };
+
+    if (allowedEmailRegexp === undefined) {
+        return null;
+    }
 
     return (
         <div className={cx(fr.cx("fr-container"), classes.root, className)}>
             <h2 className={classes.title}>{t("title")}</h2>
-            <p>
-                {t("mail")} : <span>address@foo.bar</span>
-            </p>
-            <p>
-                {t("organization")} :{" "}
-                {agencyNames.length ? (
-                    agencyNames.map(name => <span key={name}>{name}, </span>)
-                ) : (
-                    <span>{t("no organization")}</span>
-                )}
-            </p>
-            {/*TODO: Add link when auth is available*/}
-            <a href={keycloakAccountConfigurationUrl} target="_blank">
-                {t("update data")}
-            </a>
-            <Display />
+            <div className={classes.emailContainer}>
+                <Input
+                    label={t("mail")}
+                    nativeInputProps={{
+                        onChange: event =>
+                            onRequestUpdateFieldFactory("email", event.target.value),
+                        value: email
+                    }}
+                    state={isValidEmail(email).isValidValue ? undefined : "error"}
+                    stateRelatedMessage={isValidEmail(email).message}
+                />
+            </div>
+            <div>
+                <AutocompleteInput
+                    options={agencyNames}
+                    value={agencyName}
+                    onValueChange={value =>
+                        onRequestUpdateFieldFactory("agencyName", value ?? "")
+                    }
+                    getOptionLabel={entry => entry}
+                    renderOption={(liProps, entry) => (
+                        <li {...liProps}>
+                            <span>{entry}</span>
+                        </li>
+                    )}
+                    noOptionText="No result"
+                    dsfrInputProps={{
+                        "label": t("organization")
+                    }}
+                />
+            </div>
+            {keycloakAccountConfigurationUrl !== undefined && (
+                <a href={keycloakAccountConfigurationUrl} target="_blank">
+                    {t("update data")}
+                </a>
+            )}
         </div>
     );
 }
@@ -73,6 +186,9 @@ const useStyles = makeStyles({
         [fr.breakpoints.down("md")]: {
             "marginBottom": fr.spacing("8v")
         }
+    },
+    "emailContainer": {
+        marginBottom: fr.spacing("6v")
     }
 }));
 
